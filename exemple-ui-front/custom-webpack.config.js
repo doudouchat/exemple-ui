478e4ab5b6837453cd39c5b1ef92d2004d668539
doudouchat <38097871+doudouchat@users.noreply.github.com>
2021-06-17 13:39:50 +0200

    upgrade angular 12 (#28)

--- exemple-ui-front/custom-webpack.config.js
+++ exemple-ui-front/custom-webpack.config.js
@@ -1,8 +1,14 @@
+const webpack = require('webpack')
 module.exports = {
     devServer: {
         proxy: {
             '/ExempleService': 'http://localhost:8086',
             '/ExempleAuthorization': 'http://localhost:8086',
         }
-    }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        })
+    ]
 };
