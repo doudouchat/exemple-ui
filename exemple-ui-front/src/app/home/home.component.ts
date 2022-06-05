e204caee7b338d31f5f3572315f44317cc331fb5
doudouchat <baraultb@hotmail.com>
2022-06-05 08:38:52 +0100

    FIX lint failures

--- exemple-ui-front/src/app/home/home.component.ts
+++ exemple-ui-front/src/app/home/home.component.ts
@@ -1,17 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
   title = 'exemple-ui';
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
