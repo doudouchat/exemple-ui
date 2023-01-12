b1a7015d0328dc0048f9f7966ce94575b0084cc9
doudouchat <baraultb@hotmail.com>
2023-01-12 15:31:07 +0100

    upgrade authorization

--- exemple-ui-front/src/app/auth/shared/auth.service.ts
+++ exemple-ui-front/src/app/auth/shared/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpStatusCode, HttpClient, HttpHeaders, HttpParams, HttpErrorResponse 
 import { Injectable } from '@angular/core';
 import moment from 'moment';
 import { Observable } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, map, mergeMap } from 'rxjs/operators';
 
 import { Token } from '../../shared/token';
 
@@ -15,26 +15,68 @@ export class UnauthorizedError implements Error {
 @Injectable()
 export class AuthService {
 
+    private static readonly LOCATION_MATCHER = /.*code=([a-zA-Z0-9\-_]*)(&state=)?/g;
+
     constructor(private readonly http: HttpClient) { }
 
     clientCredentials(clientId: string, secret: string): Observable<Token> {
 
         const body = new HttpParams()
-            .append('grant_type', 'client_credentials');
+            .append('grant_type', 'client_credentials')
+            .append('scope', 'account:create login:head login:create ROLE_APP');
 
         return this.token(clientId, secret, body);
     }
 
     password(clientId: string, secret: string, username: string, password: string): Observable<Token> {
 
-        const body = new HttpParams()
-            .append('grant_type', 'password')
+        const loginData = new HttpParams()
             .append('username', username)
-            .append('password', password)
-            .append('client_id', clientId)
-            .append('redirect_uri', 'xxx');
+            .append('password', password);
 
-        return this.token(clientId, secret, body);
+        return this.http.post<Token>('/ExempleAuthorization/login',
+            loginData,
+            {
+                headers: new HttpHeaders({
+                    'app': 'test'
+                }),
+                observe: 'response'
+            }).pipe(
+                mergeMap(loginResponse => {
+                    const authorizationData = new HttpParams()
+                        .append('response_type', 'code')
+                        .append('client_id', 'test_service_user')
+                        .append('scope', 'account:read account:update login:head login:read login:create login:update');
+                    const xAuthToken = loginResponse.headers.get('x-auth-token');
+                    return this.http.get<void>('/ExempleAuthorization/oauth/authorize',
+                        {
+                            params: authorizationData,
+                            headers: { 'x-auth-token': xAuthToken },
+                            observe: 'response'
+                        }
+                    ).pipe(mergeMap(authorizeResponse => {
+
+                        const location = authorizeResponse.headers.get('location');
+
+                        const matches = AuthService.LOCATION_MATCHER.exec(location);
+                        const code = matches[1];
+
+                        const authorization = new HttpParams()
+                            .append('grant_type', 'authorization_code')
+                            .append('code', code)
+                            .append('client_id', 'test_service_user');
+
+                        return this.token(clientId, secret, authorization);
+                    }));
+                }),
+                catchError((error: HttpErrorResponse) => {
+
+                    if (error.status === HttpStatusCode.Unauthorized) {
+                        throw new UnauthorizedError();
+                    }
+
+                    throw error;
+                }));
 
     }
 
@@ -51,14 +93,6 @@ export class AuthService {
                 map((t: Token) => {
                     t.expires_in = t.expires_in / moment.duration(1, 'days').asSeconds();
                     return t;
-                }),
-                catchError((error: HttpErrorResponse) => {
-
-                    if (error.status === HttpStatusCode.Unauthorized) {
-                        throw new UnauthorizedError();
-                    }
-
-                    throw error;
                 })
             );
     }
