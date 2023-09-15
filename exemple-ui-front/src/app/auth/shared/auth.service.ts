c6562a6f1371987492ab8e5387bd45c2f4d18dc4
doudouchat <baraultb@hotmail.com>
2023-09-15 08:02:43 +0100

    refacto authentication service

--- exemple-ui-front/src/app/auth/shared/auth.service.ts
+++ exemple-ui-front/src/app/auth/shared/auth.service.ts
@@ -5,106 +5,113 @@ import Base64url from 'crypto-js/enc-base64url';
 import moment from 'moment';
 import { Observable } from 'rxjs';
 import { catchError, map, mergeMap } from 'rxjs/operators';
-import { v4 as uuidv4 } from 'uuid';
 
 import { Token } from '../../shared/token';
 
 export class UnauthorizedError implements Error {
 
-    readonly name = 'UnauthorizedError';
-    readonly message: string;
+  readonly name = 'UnauthorizedError';
+  readonly message: string;
 }
 
 @Injectable()
 export class AuthService {
 
-    private static readonly LOCATION_MATCHER = /.*code=([a-zA-Z0-9\-_]*)(&state=)?/g;
+  private static readonly LOCATION_MATCHER = /.*code=([a-zA-Z0-9\-_]*)(&state=)?/g;
 
-    constructor(private readonly http: HttpClient) { }
+  constructor(private readonly http: HttpClient) { }
 
-    clientCredentials(clientId: string, secret: string): Observable<Token> {
+  authenticateApplication(): Observable<Token> {
 
-        const body = new HttpParams()
-            .append('grant_type', 'client_credentials')
-            .append('scope', 'account:create login:head login:create ROLE_APP');
+    const body = new HttpParams()
+      .append('grant_type', 'client_credentials')
+      .append('scope', 'account:create login:head login:create ROLE_APP');
 
-        return this.token(body, clientId, secret);
-    }
+    return this.token(body, 'test_service', 'secret');
+  }
 
-    password(username: string, password: string): Observable<Token> {
+  authenticateUser(username: string, password: string): Observable<Token> {
 
-        const loginData = new HttpParams()
-            .append('username', username)
-            .append('password', password);
+    const loginData = new HttpParams()
+      .append('username', username)
+      .append('password', password);
 
-        const codeVerifier = uuidv4();
-        const codeChallenge = Base64url.stringify(sha256(codeVerifier));
+    const codeVerifier = this.generateCodeVerifier();
+    const codeChallenge = this.generateCodeChallenge(codeVerifier)
 
-        return this.http.post<Token>('/ExempleAuthorization/login',
-            loginData,
+    return this.http.post<Token>('/ExempleAuthorization/login',
+      loginData,
+      {
+        headers: new HttpHeaders({
+          'app': 'test'
+        }),
+        observe: 'response'
+      }).pipe(
+        mergeMap(loginResponse => {
+          const authorizationData = new HttpParams()
+            .append('response_type', 'code')
+            .append('client_id', 'test_service_user')
+            .append('code_challenge', codeChallenge)
+            .append('code_challenge_method', 'S256')
+            .append('scope', 'account:read account:update login:head login:read login:create login:update');
+          const xAuthToken = loginResponse.headers.get('x-auth-token');
+          return this.http.get<void>('/ExempleAuthorization/oauth/authorize',
             {
-                headers: new HttpHeaders({
-                    'app': 'test'
-                }),
-                observe: 'response'
-            }).pipe(
-                mergeMap(loginResponse => {
-                    const authorizationData = new HttpParams()
-                        .append('response_type', 'code')
-                        .append('client_id', 'test_service_user')
-                        .append('code_challenge', codeChallenge)
-                        .append('code_challenge_method', 'S256')
-                        .append('scope', 'account:read account:update login:head login:read login:create login:update');
-                    const xAuthToken = loginResponse.headers.get('x-auth-token');
-                    return this.http.get<void>('/ExempleAuthorization/oauth/authorize',
-                        {
-                            params: authorizationData,
-                            headers: { 'x-auth-token': xAuthToken },
-                            observe: 'response'
-                        }
-                    ).pipe(mergeMap(authorizeResponse => {
+              params: authorizationData,
+              headers: { 'x-auth-token': xAuthToken },
+              observe: 'response'
+            }
+          ).pipe(mergeMap(authorizeResponse => {
 
-                        const location = authorizeResponse.headers.get('location');
+            const location = authorizeResponse.headers.get('location');
 
-                        const matches = AuthService.LOCATION_MATCHER.exec(location);
-                        const code = matches[1];
+            const matches = AuthService.LOCATION_MATCHER.exec(location);
+            const code = matches[1];
 
-                        const authorization = new HttpParams()
-                            .append('grant_type', 'authorization_code')
-                            .append('code', code)
-                            .append('code_verifier', codeVerifier)
-                            .append('client_id', 'test_service_user');
+            const authorization = new HttpParams()
+              .append('grant_type', 'authorization_code')
+              .append('code', code)
+              .append('code_verifier', codeVerifier)
+              .append('client_id', 'test_service_user');
 
-                        return this.token(authorization);
-                    }));
-                }),
-                catchError((error: HttpErrorResponse) => {
+            return this.token(authorization);
+          }));
+        }),
+        catchError((error: HttpErrorResponse) => {
 
-                    if (error.status === HttpStatusCode.Unauthorized) {
-                        throw new UnauthorizedError();
-                    }
+          if (error.status === HttpStatusCode.Unauthorized) {
+            throw new UnauthorizedError();
+          }
 
-                    throw error;
-                }));
+          throw error;
+        }));
 
+  }
+
+  private token(body: HttpParams, clientId?: string, secret?: string): Observable<Token> {
+
+    let httpHeaders;
+    if (clientId) {
+      httpHeaders = new HttpHeaders({
+        'Content-type': 'application/x-www-form-urlencoded; charset=utf-8',
+        Authorization: 'Basic ' + btoa(`${clientId}:${secret}`)
+      })
     }
 
-    private token(body: HttpParams, clientId?: string, secret?: string): Observable<Token> {
-
-      let httpHeaders;
-      if (clientId) {
-        httpHeaders = new HttpHeaders({
-          'Content-type': 'application/x-www-form-urlencoded; charset=utf-8',
-          Authorization: 'Basic ' + btoa(`${clientId}:${secret}`)
+    return this.http.post<Token>('/ExempleAuthorization/oauth/token', body, { headers: httpHeaders })
+      .pipe(
+        map((t: Token) => {
+          t.expires_in = t.expires_in / moment.duration(1, 'days').asSeconds();
+          return t;
         })
-      }
-
-      return this.http.post<Token>('/ExempleAuthorization/oauth/token', body, { headers: httpHeaders })
-        .pipe(
-          map((t: Token) => {
-            t.expires_in = t.expires_in / moment.duration(1, 'days').asSeconds();
-            return t;
-          })
       );
+  }
+
+  private generateCodeChallenge(codeVerifier: string): string {
+    return Base64url.stringify(sha256(codeVerifier));
+  }
+
+  private generateCodeVerifier(): string {
+    return crypto.randomUUID();
   }
 }
--- exemple-ui-front/src/app/auth/shared/auth.state.ts
+++ exemple-ui-front/src/app/auth/shared/auth.state.ts
@@ -6,30 +6,30 @@ import { Authenticate, Logout } from './auth.action';
 import { AuthService } from './auth.service';
 
 export interface AuthStateModel {
-    authenticate: boolean;
-    username?: string;
+  authenticate: boolean;
+  username?: string;
 }
 
 @State<AuthStateModel>({
-    name: 'authenticate',
-    defaults: { authenticate: false }
+  name: 'authenticate',
+  defaults: { authenticate: false }
 })
 @Injectable()
 export class AuthState {
 
-    constructor(
-        private readonly authService: AuthService) { }
+  constructor(
+    private readonly authService: AuthService) { }
 
-    @Action(Authenticate)
-    authenticate(ctx: StateContext<AuthStateModel>, action: Authenticate) {
-        return this.authService.password(action.username, action.password).pipe(
-            tap(() => {
-                ctx.setState({ authenticate: true, username: action.username });
-            }));
-    }
+  @Action(Authenticate)
+  authenticate(ctx: StateContext<AuthStateModel>, action: Authenticate) {
+    return this.authService.authenticateUser(action.username, action.password).pipe(
+      tap(() => {
+        ctx.setState({ authenticate: true, username: action.username });
+      }));
+  }
 
-    @Action(Logout)
-    logout(ctx: StateContext<AuthStateModel>) {
-        ctx.setState({ authenticate: false });
-    }
+  @Action(Logout)
+  logout(ctx: StateContext<AuthStateModel>) {
+    ctx.setState({ authenticate: false });
+  }
 }
