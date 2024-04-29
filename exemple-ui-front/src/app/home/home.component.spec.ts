79621b5bc89e1d6966b903b8a02894e96d0e4e89
doudouchat <baraultb@hotmail.com>
2024-04-29 20:22:48 +0100

    UT to check link to account

--- exemple-ui-front/src/app/home/home.component.spec.ts
+++ exemple-ui-front/src/app/home/home.component.spec.ts
@@ -1,4 +1,4 @@
-import { DebugElement } from '@angular/core';
+import { Component, DebugElement } from '@angular/core';
 import { TestBed, ComponentFixture, waitForAsync } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { By } from '@angular/platform-browser';
@@ -7,20 +7,31 @@ import { expect } from 'chai';
 
 import { HomeComponent } from './home.component';
 
+@Component({
+  template: '<h6>dummy</h6>',
+  standalone: true
+})
+class DummyComponent {
+}
+
 describe('HomeComponent', () => {
 
   let fixture: ComponentFixture<HomeComponent>;
+  let mock: ComponentFixture<DummyComponent>;
 
   beforeEach(waitForAsync(() => {
 
     fixture = TestBed.configureTestingModule({
 
       imports: [
+        DummyComponent,
         HttpClientTestingModule,
-        RouterModule.forRoot([])
+        RouterModule.forRoot(
+          [{ path: 'account', component: DummyComponent }])
       ]
 
     }).createComponent(HomeComponent);
+    mock = TestBed.createComponent(DummyComponent);
 
   }));
 
@@ -34,9 +45,23 @@ describe('HomeComponent', () => {
 
     fixture.detectChanges();
 
-    const de : DebugElement[] = fixture.debugElement.queryAll(By.css('h1'));
+    const de: DebugElement[] = fixture.debugElement.queryAll(By.css('h1'));
 
     expect(de[0].nativeElement.innerHTML).to.equal(' Welcome to exemple-ui! ');
+
+  }));
+
+  it('home have link to account', waitForAsync(() => {
+
+    fixture.detectChanges();
+
+    // When click link
+    const link: DebugElement = fixture.debugElement.query(By.css('a[routerLink="/account"]'));
+    link.nativeElement.click();
+
+    // Then check content
+    const de: DebugElement[] = mock.debugElement.queryAll(By.css('h6'));
+    expect(de[0].nativeElement.innerHTML).to.equal('dummy');
 
   }));
 
