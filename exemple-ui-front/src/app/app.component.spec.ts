6061070aad78d57591ec2dc238899ac310fcf2fd
doudouchat <baraultb@hotmail.com>
2024-05-02 19:40:59 +0100

    review app UT

--- exemple-ui-front/src/app/app.component.spec.ts
+++ exemple-ui-front/src/app/app.component.spec.ts
@@ -50,91 +50,86 @@ describe('AppComponent', () => {
 
   });
 
-  it('routing should have as template dummy', waitForAsync(inject(
-    [HttpTestingController], (http) => {
+  describe('Forward to any template', () => {
 
-      const req = http.expectOne({ method: 'POST', url: '/ExempleAuthorization/oauth/token' });
-      req.flush({
-        expires_in: 300
-      });
-      http.expectNone({ method: 'GET', url: '/ExempleService/ws/v1/logins/john.doe@gmail.com' });
-      http.verify();
+    it('routing should have as template dummy', waitForAsync(inject(
+      [HttpTestingController], (http) => {
 
-      fixture.detectChanges();
+        // Setup authenticate
+        const req = http.expectOne({ method: 'POST', url: '/ExempleAuthorization/oauth/token' });
+        req.flush({
+          expires_in: 300
+        });
+        http.expectNone({ method: 'GET', url: '/ExempleService/ws/v1/logins/john.doe@gmail.com' });
+        http.verify();
 
-      expect(store.selectSnapshot(state => state.application)).is.be.true;
+        fixture.detectChanges();
 
-      mock.detectChanges();
-
-      mock.whenStable().then(() => {
-
-        mock.detectChanges();
-        const de: DebugElement[] = mock.debugElement.queryAll(By.css('h6'));
-
-        expect(de[0].nativeElement.innerHTML).to.equal('dummy');
-
-      });
-
-    })));
-
-  it('routing should have as template dummy refresh', waitForAsync(inject(
-    [HttpTestingController], (http) => {
-
-      http.expectNone({ method: 'POST', url: '/ExempleAuthorization/oauth/token' });
-      const getLogin = http.expectOne({ method: 'GET', url: '/ExempleService/ws/v1/logins/john.doe@gmail.com' });
-      getLogin.flush(99);
-      const getAccount = http.expectOne({ method: 'GET', url: '/ExempleService/ws/v1/accounts/99' });
-      getAccount.flush({
-        firstname: 'john',
-        lastname: 'doe'
-      });
-      http.verify();
-
-      fixture.detectChanges();
-
-      expect(store.selectSnapshot(state => state.application)).is.be.undefined;
-      expect(store.selectSnapshot(state => state.authenticate.authenticate)).is.be.true;
-      expect(store.selectSnapshot(state => state.authenticate.username)).is.be.eq('john.doe@gmail.com');
-
-      mock.detectChanges();
-
-      mock.whenStable().then(() => {
+        // Then check state
+        expect(store.selectSnapshot(state => state.application)).is.be.true;
 
         mock.detectChanges();
-        const de: DebugElement[] = mock.debugElement.queryAll(By.css('h6'));
 
+        // And check dummy
+        const de: DebugElement[] = mock.debugElement.queryAll(By.css('h6'));
         expect(de[0].nativeElement.innerHTML).to.equal('dummy');
 
-      });
+      })));
 
-    })));
+    it('routing should have as template dummy refresh', waitForAsync(inject(
+      [HttpTestingController], (http) => {
 
-  it('routing should have as template dummy refresh exception', waitForAsync(inject(
-    [HttpTestingController], (http) => {
+        // Setup authenticate
+        http.expectNone({ method: 'POST', url: '/ExempleAuthorization/oauth/token' });
+        const getLogin = http.expectOne({ method: 'GET', url: '/ExempleService/ws/v1/logins/john.doe@gmail.com' });
+        getLogin.flush(99);
+        const getAccount = http.expectOne({ method: 'GET', url: '/ExempleService/ws/v1/accounts/99' });
+        getAccount.flush({
+          firstname: 'john',
+          lastname: 'doe'
+        });
+        http.verify();
 
-      http.expectNone({ method: 'POST', url: '/ExempleAuthorization/oauth/token' });
-      const getLogin = http.expectOne({ method: 'GET', url: '/ExempleService/ws/v1/logins/john.doe@gmail.com' });
-      getLogin.flush({}, { status: 403, statusText: 'access is forbidden' });
-      http.expectNone({ method: 'GET', url: '/ExempleService/ws/v1/accounts/99' });
-      http.verify();
+        fixture.detectChanges();
 
-      fixture.detectChanges();
-
-      expect(store.selectSnapshot(state => state.application)).is.be.undefined;
-      expect(store.selectSnapshot(state => state.authenticate.authenticate)).is.be.false;
-      expect(store.selectSnapshot(state => state.authenticate.username)).is.be.undefined;
-
-      mock.detectChanges();
-
-      mock.whenStable().then(() => {
+        // Then check state
+        expect(store.selectSnapshot(state => state.application)).is.be.undefined;
+        expect(store.selectSnapshot(state => state.authenticate.authenticate)).is.be.true;
+        expect(store.selectSnapshot(state => state.authenticate.username)).is.be.eq('john.doe@gmail.com');
 
         mock.detectChanges();
-        const de: DebugElement[] = mock.debugElement.queryAll(By.css('h6'));
 
+        // And check dummy
+        const de: DebugElement[] = mock.debugElement.queryAll(By.css('h6'));
         expect(de[0].nativeElement.innerHTML).to.equal('dummy');
 
-      });
+      })));
 
-    })));
+    it('routing should have as template dummy refresh exception', waitForAsync(inject(
+      [HttpTestingController], (http) => {
+
+        // Setup authenticate
+        http.expectNone({ method: 'POST', url: '/ExempleAuthorization/oauth/token' });
+        const getLogin = http.expectOne({ method: 'GET', url: '/ExempleService/ws/v1/logins/john.doe@gmail.com' });
+        getLogin.flush({}, { status: 403, statusText: 'access is forbidden' });
+        http.expectNone({ method: 'GET', url: '/ExempleService/ws/v1/accounts/99' });
+        http.verify();
+
+        fixture.detectChanges();
+
+        // Then check state
+        expect(store.selectSnapshot(state => state.application)).is.be.undefined;
+        expect(store.selectSnapshot(state => state.authenticate.authenticate)).is.be.false;
+        expect(store.selectSnapshot(state => state.authenticate.username)).is.be.undefined;
+
+        mock.detectChanges();
+
+        // And check dummy
+        const de: DebugElement[] = mock.debugElement.queryAll(By.css('h6'));
+        expect(de[0].nativeElement.innerHTML).to.equal('dummy');
+
+      })));
+
+  });
 
 });
