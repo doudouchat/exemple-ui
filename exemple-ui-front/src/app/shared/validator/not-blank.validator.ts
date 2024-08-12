9a78a29515c244ebcaf4f8db8fb25fbab208f11d
doudouchat <baraultb@hotmail.com>
2024-08-12 10:39:47 +0100

    upgrade eslint 9.9.0

--- exemple-ui-front/src/app/shared/validator/not-blank.validator.ts
+++ exemple-ui-front/src/app/shared/validator/not-blank.validator.ts
@@ -5,6 +5,7 @@ export function notBlank(): ValidatorFn {
   const NOT_BLANK_REGEXP = /^(?!\s*$)./i;
 
   /* eslint-disable @typescript-eslint/no-explicit-any */
+  /* eslint-disable @typescript-eslint/consistent-indexed-object-style */
   return (control: AbstractControl): { [key: string]: any } | null =>
     !NOT_BLANK_REGEXP.test(control.value) ? { notBlank: { value: control.value } } : null;
 }
